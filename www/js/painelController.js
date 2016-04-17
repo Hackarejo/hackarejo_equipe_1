222f0b183cbc9fa9234b112ba14b308782bf25cf
Rafael Silva <95rafael.silva@gmail.com>
2016-04-17 04:28:39 -0300

    Ajustes na tela

--- www/js/painelController.js
+++ www/js/painelController.js
@@ -1,5 +1,10 @@
-app.controller('PainelController', function($scope, $http, $location) {
+app.controller('PainelController', function($scope, $http, $location, $rootScope) {
 	$http.get("http://172.40.0.63:8080/hackarejo/ws/listarEstabelecimentos").then(function(value) {
 		$scope.listaEstabelecimentos = value.data;
 	});
+
+	$scope.abrirRestaurante = function(restaurante) {
+		$rootScope.restaurante = restaurante;
+		$location.path("/restaurante");
+	};
 });
--- www/js/restauranteController.js
+++ www/js/restauranteController.js
@@ -1,3 +1,9 @@
-app.controller('RestauranteController', function($scope, $http, $location) {
-
+app.controller('RestauranteController', function($scope, $http, $location, $rootScope) {
+	
+	if($rootScope.restaurante != undefined) {
+		$scope.restaurante = $rootScope.restaurante;
+		console.log($scope.restaurante);
+	}
+	
+	
 });
